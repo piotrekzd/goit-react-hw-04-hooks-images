97ec2d1cbf2e824d37c3100416fc4e95f757b803
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-11-13 21:48:51 +0100

    Update API.js

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -2,9 +2,16 @@ import axios from "axios";
 
 const API_KEY = '29677253-331af6074919a5bc21fef774b';
 
-export const fetchImg = async (query, page, perPage) => {
+export const fetchImg = async (query, page) => {
     const response = await axios.get
-        (`https://pixabay.com/api/?key=${API_KEY}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=${perPage}&safesearch=true`);
+        (`https://pixabay.com/api/?key=${API_KEY}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=12&safesearch=true`);
     // console.log(response.data);
-    return response.data;
+    return response.data.hits.map(image => {
+        return {
+            id: image.id,
+            webformatURL: image.webformatURL,
+            largeImageURL: image.largeImageURL,
+            tags: image.tags
+        };
+    });
 };
